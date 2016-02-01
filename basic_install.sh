89cda99bdc980905fd77182dc8759f0e30d589b4
mrodriguez <mrodriguez@uvsouthsourcing.net>
2016-02-01 17:22:16 -0300

    new pkg

--- basic_install.sh
+++ basic_install.sh
@@ -1,7 +1,21 @@
 sudo locale-gen en_US en_US.UTF-8 
 
-cd /tmp
 
+#minimal server pkgs
+sudo apt-get install -y \
+	#apache \
+	#mysql \
+	ctags \
+	htop \
+	nginx \
+	convert \
+	tmux \
+	zsh  \
+	vim \
+	fontconfig
+
+
+cd /tmp
 git clone git://github.com/trapd00r/ls--.git
 git clone https://github.com/w4rlock/servers_dotfiles.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -10,11 +24,22 @@ cp servers_dotfiles/.* ~/
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
 
+
+#powered ls
 cd ls--
 sudo cpan Term::ExtendedColor
 perl Makefile.PL
 make 
 sudo make install
 
-sudo apt-get install -y ctags htop 
-npm i -g forever pm2
+
+
+#node js tools
+npm i -g \
+	forever \
+	pm2 \
+	nodemon \
+	grunt \
+	gulp \
+	bower
+
